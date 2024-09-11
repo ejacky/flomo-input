1f8f823839f3c97680f431d48482955d46ae57c4
jackyzhang <852566802@qq.com>
2024-09-12 07:32:18 +0800

    init

--- src/background.js
+++ src/background.js
@@ -5,17 +5,14 @@
 // For more information on background script,
 // See https://developer.chrome.com/extensions/background_pages
 
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.type === 'GREETINGS') {
-    const message = `Hi ${
-      sender.tab ? 'Con' : 'Pop'
-    }, my name is Bac. I am from Background. It's great to hear from you.`;
 
-    // Log message coming from the `request` parameter
-    console.log(request.payload.message);
-    // Send a response message
-    sendResponse({
-      message,
-    });
-  }
+chrome.action.onClicked.addListener((tab) => {
+  chrome.windows.create({
+    url: chrome.runtime.getURL("floating.html"),
+    type: "popup",
+    width: 350,
+    height: 300,
+    left: screen.width - 350,
+    top: 100
+  });
 });
